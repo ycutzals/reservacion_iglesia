6c3a5d530f5baddc02b9db376864f585d8bff018
unknown <yefrycutzalsimilox@gmail.com>
2023-02-22 23:41:02 -0600

    cambios prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-  <!-- Styles -->
+  <!-- sStyles -->
 
 </head>
 
